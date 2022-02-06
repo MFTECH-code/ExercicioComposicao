a6fcdc8602f792ad519351814e44d94719a3e4c8
MFTECH-code <matheussilvafeitosa78@gmail.com>
2022-02-06 01:24:48 -0300

    Implementing Method

--- ExerciseComposition1/Worker.cs
+++ ExerciseComposition1/Worker.cs
@@ -8,6 +8,16 @@ namespace ExerciseComposition1
         public WorkerLevel Level { get; set; }
         public double BaseSalary { get; set; }
         
-        public IList<HourContract> Contracts { get; } 
+        public IList<HourContract> Contracts { get; }
+
+        public Worker()
+        {
+            Contracts = new List<HourContract>();
+        }
+        
+        public void AddContract(HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
     }
 }
