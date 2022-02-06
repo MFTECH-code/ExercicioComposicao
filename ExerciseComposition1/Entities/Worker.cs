2e0798f64bd589bea209ea7b4f60967d79fb1666
MFTECH-code <matheussilvafeitosa78@gmail.com>
2022-02-06 02:03:08 -0300

    Implementig method

--- ExerciseComposition1/Entities/Worker.cs
+++ ExerciseComposition1/Entities/Worker.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using ExerciseComposition1.Entities.Enums;
 
 namespace ExerciseComposition1.Entities
@@ -24,6 +25,20 @@ namespace ExerciseComposition1.Entities
         public void RemoveContract(int indexer)
         {
             Contracts.RemoveAt(indexer);
+        }
+
+        public double Income(int year, int month)
+        {
+            var calcContracts = 0.0;
+            foreach (var contract in Contracts)
+            {
+                if (contract.Date.Year == year && contract.Date.Month == month)
+                {
+                    calcContracts += contract.TotalValue();
+                }
+            }
+
+            return BaseSalary + calcContracts;
         }
     }
 }
