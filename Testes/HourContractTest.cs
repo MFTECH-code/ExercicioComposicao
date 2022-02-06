dbc4a2d0fbb00437dc5c077ccced5dd47bf005f8
MFTECH-code <matheussilvafeitosa78@gmail.com>
2022-02-06 02:05:57 -0300

    Implementig Test

---
+++ Testes/HourContractTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ExerciseComposition1.Entities;
+using Xunit;
+
+namespace Testes
+{
+    public class HourContractTest
+    {
+        [Fact]
+        public void TestValorTotalDoContrato()
+        {
+            HourContract falseHourContract = new HourContract
+            {
+                Date = new DateTime(2022, 07, 30),
+                Hours = 40,
+                ValuePerHour = 30.00
+            };
+
+            var result = falseHourContract.TotalValue();
+            
+            Assert.Equal(1200, result);
+        }
+    }
+}
